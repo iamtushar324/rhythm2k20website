18acc95a8a7924ec783ecd9c4b054f271ad804ea
Tushar Bhardwaj <iamtushar324@gmail.com>
2020-01-30 22:46:32 +0530

    modified btn
    Signed-off-by: Tushar Bhardwaj <iamtushar324@gmail.com>

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let bar3 = document.getElementsByClassName('bar3')[0]
 
 
 let is_menu_on = false
+
 menu_btn.onclick = () => {
     if (is_menu_on) {
         menu_list.style.left = "100%"
@@ -13,7 +14,7 @@ menu_btn.onclick = () => {
 
         bar1.style.transform = "rotateZ(0deg)"
         bar3.style.transform = "rotateZ(0deg)"
-        bar2.style.transform = "translateX(0px)"
+        bar2.style.opacity = "1"
 
 
 
@@ -23,7 +24,7 @@ menu_btn.onclick = () => {
         menu_list.style.left = "60%"
         bar1.style.transform = "rotateZ(-33deg)"
         bar3.style.transform = "rotateZ(33deg)"
-        bar2.style.transform = "translateX(100px)"
+        bar2.style.opacity = "0"
 
         is_menu_on = true
 
