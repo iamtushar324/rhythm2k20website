51df09f8565ef3c413f14ad7c9f5e57a8c9bf159
Tushar Bhardwaj <iamtushar324@gmail.com>
2020-01-30 22:30:27 +0530

    menu-btn animation for phone
    Signed-off-by: Tushar Bhardwaj <iamtushar324@gmail.com>

---
+++ index.js
@@ -0,0 +1,32 @@
+let menu_btn = document.getElementById('menu-btn')
+let menu_list = document.getElementById('menu-list')
+let bar1 = document.getElementsByClassName('bar1')[0]
+let bar2 = document.getElementsByClassName('bar2')[0]
+let bar3 = document.getElementsByClassName('bar3')[0]
+
+
+let is_menu_on = false
+menu_btn.onclick = () => {
+    if (is_menu_on) {
+        menu_list.style.left = "100%"
+        is_menu_on = false
+
+        bar1.style.transform = "rotateZ(0deg)"
+        bar3.style.transform = "rotateZ(0deg)"
+        bar2.style.transform = "translateX(0px)"
+
+
+
+
+    }
+    else {
+        menu_list.style.left = "60%"
+        bar1.style.transform = "rotateZ(-33deg)"
+        bar3.style.transform = "rotateZ(33deg)"
+        bar2.style.transform = "translateX(100px)"
+
+        is_menu_on = true
+
+    }
+
+}
